fd2688da352caf2eca82a4d14ca71f8f25ba0eed
Brian Ordonez <bordonez1129@gmail.com>
2025-03-05 13:47:50 -0500

    Changes
    Changes........

--- ARM_InverseKinematics/ARM_InverseKinematics.ino
+++ ARM_InverseKinematics/ARM_InverseKinematics.ino
@@ -1,7 +1,28 @@
+#include <Arduino.h>
+#include <math.h>
+
 void setup() {
+  const float L1 = 2.598;
+  const float L2 = 2.44;
+  const float L3 = 6.649;
+  const float L4 = 7.071;
+  const float L5 = 0.74;
+  const float L6 = 1.00;   
+
+  const float joint_limits[6][2] =
+  {
+    {-5.0, 180.0},
+    {-110.0, 110.0},
+    {-180, 90},
+    {-5, 120},
+    {-90, 90},
+    {-40, 100}
+  };
+
   
+           
 }
 
 void loop() {
-  
+
 }
